b380e6fab4bd92e699a55862c75596f9ecc44913
Kalaiprabha <KALAIPRABHA.L@cognizant.com>
2015-08-03 15:37:45 +0530

    Initial Commit

---
+++ layoutTrial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  layoutTrial
+//
+//  Created by cts on 03/08/15.
+//  Copyright (c) 2015 asdasdsd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ layoutTrial/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  layoutTrial
+//
+//  Created by cts on 03/08/15.
+//  Copyright (c) 2015 asdasdsd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
